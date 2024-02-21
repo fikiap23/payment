07b4acbd4fd0c4b62498d078d467ff26f5ce7827
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-21 23:02:11 +0700

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes/index.js';
-import { errorHandler } from './middleware/error-handler.js';
+import express from 'express'
+import cors from 'cors'
+import routes from './routes/index.js'
+import { errorHandler } from './middleware/error-handler.js'
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express()
+const PORT = process.env.PORT || 8000
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.use(routes);
+app.use(routes)
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
