84b7cdec60ba952a3feb1734277ebac378cf59d6
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-24 13:28:45 +0700

    add header

--- frontend/src/pages/Home/Header.jsx
+++ frontend/src/pages/Home/Header.jsx
@@ -1,15 +1,18 @@
-import { useNavigate } from 'react-router-dom';
-import historyImage from './ic-order-status.svg';
+import { useNavigate } from 'react-router-dom'
+import historyImage from './ic-order-status.svg'
 
 export const Header = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    return (
-      <div className='header-banner'>
-        <p className='logo-title'>Ndoro Store</p>
-        <div className='icon-check-order' onClick={() => navigate('/order-status')}>
-          <img src={historyImage} alt='order-status' />
-        </div>
+  return (
+    <div className="header-banner">
+      <p className="logo-title">FA-Store</p>
+      <div
+        className="icon-check-order"
+        onClick={() => navigate('/order-status')}
+      >
+        <img src={historyImage} alt="order-status" />
       </div>
-    )
+    </div>
+  )
 }
